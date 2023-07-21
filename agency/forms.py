62245b8cd333dce6a3e7735024128c4343e0113e
yakhlamova <yanaakhlamova@gmail.com>
2023-07-21 09:25:25 +0300

    Added forms

---
+++ agency/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from agency.models import Redactor, Newspaper
+
+
+class RedactorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "years_of_experience",
+            "first_name",
+            "last_name"
+        )
+
+
+class NewspaperForm(forms.ModelForm):
+    redactors = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Newspaper
+        fields = "__all__"
